254995fd6b955370a9ddc63f7834db210b5df93d
matthenschke <matthewhenschke98@gmail.com>
2018-08-18 09:38:02 -0400

    Added events that copy drawings on other socket connections

--- app.js
+++ app.js
@@ -17,5 +17,12 @@ io.sockets.on("connection", newConnection);
 
 function newConnection(socket){
     console.log('new connection'+ socket.id);
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data){
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    }
+
 
 }
--- public/sketch.js
+++ public/sketch.js
@@ -5,10 +5,32 @@ function setup() {
         var y = (windowHeight - height) / 2;
         cnv.position(x, y);
         background(255, 0, 200);
+        
         socket = io.connect('http://localhost:8000');
+        socket.on('mouse', newDrawing);
       }
-function draw(){
+
+//function that mirrors drawing in other connections to socket
+function newDrawing(data){
+    noStroke();
+    fill(255,0,100); 
+    ellipse(data.x, data.y, 60, 60);
+
+}
+function mouseDragged(){
+    console.log('Sending ' + mouseX + ','+ mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    };
+    socket.emit('mouse', data);
+    
+    
     noStroke();
     fill(150); 
     ellipse(mouseX, mouseY, 60, 60);
+}
+function draw(){
+    
 }
